adde4eabe6bd73519b37b5dbb7073ee8d9f02f93
Daniel Josué <daniel.narvaez@smartdimension.co>
2024-05-24 21:42:41 -0500

    Co-authored-by: JCamiloLopezF <JCamiloLopezF@users.noreply.github.com>

--- appmatdiscretas/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module co.uniquindio.edu.javafx.discretas {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens co.uniquindio.edu.javafx.discretas to javafx.fxml;
-    exports co.uniquindio.edu.javafx.discretas;
-}
