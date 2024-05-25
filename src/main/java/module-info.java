6d25aa90a1e4e944d9dd3a4a8faeb141577342cf
Daniel Josué <daniel.narvaez@smartdimension.co>
2024-05-24 22:29:43 -0500

    Cambios

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,10 @@ module co.uniquindio.edu.dicretas {
 
     opens co.uniquindio.edu.dicretas to javafx.fxml;
     exports co.uniquindio.edu.dicretas;
+
+    opens co.uniquindio.edu.dicretas.Controller;
+    exports co.uniquindio.edu.dicretas.Controller;
+
+    opens co.uniquindio.edu.dicretas.Model;
+    exports co.uniquindio.edu.dicretas.Model;
 }
